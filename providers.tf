8f101f3034ee32186630fc60fd1324f4334718ad
Shaun S <shaun.stuart@gmail.com>
2022-10-18 15:52:53 +1300

    refactor folder and files

--- providers.tf
+++ providers.tf
@@ -1,8 +1,18 @@
-resource "random_pet" "rg_name" {
-  prefix = var.resource_group_name_prefix
+terraform {
+  required_version = ">=0.12"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>2.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
+    }
+  }
 }
 
-resource "azurerm_resource_group" "rg" {
-  location = var.resource_group_location
-  name     = random_pet.rg_name.id
+provider "azurerm" {
+  features {}
 }
